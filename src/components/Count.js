feebccf94cc401163164ba0732f8b2e8159f1413
elenareach@gmail.com <elenareach@gmail.com>
2023-10-16 17:33:29 -0600

    update design for count Page

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,4 +1,5 @@
 import React , {useState} from "react";
+import {BlueButton} from "../utils/styleConsts"
 
 const Count=()=>{
     const [count, setCount]  = useState(0 );
@@ -14,20 +15,23 @@ const Count=()=>{
     }
 
     return( 
-        <div className="boxContainer cardContainer">
-            <div className="card dark">
-                <div className="container light">
-                    <div className="title titleColor">
-                            <h1>Count</h1>
-                    </div>
-                    
-                    <div className="bottom">
-                        <button onClick={decrimentCount}>-</button>
-                            <span className="text">{count}</span>
-                        <button onClick={incrementCount}>+</button>
-                    </div>
+        <>
+             <header class="bg-white shadow">
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Count</h1>
                 </div>
-            </div>
-            </div>);
+            </header>
+            <main>
+                <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">       
+                    <div className="flex flex-row gap-5">
+                        <button className={BlueButton} onClick={decrimentCount}>-</button>
+                        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{count}</h1>
+                        <button className={BlueButton} onClick={incrementCount}>+</button>
+                    </div>
+                       
+                </div>
+            </main>
+        </>
+     );
 }
 export default Count;
